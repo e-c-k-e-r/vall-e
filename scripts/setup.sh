2f2505b12f93f8c71c1c08e2a0b0f28512cd750e
mrq <mrq@ecker.tech>
2023-10-06 08:08:28 -0500

    updated setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,8 @@ python3 -m venv venv
 pip3 install -e .
 
 mkdir -p ./training/valle/ckpt/ar+nar-retnet-8/
-wget -P ./training/valle/ckpt/ar+nar-retnet-8/fp32.pth "https://huggingface.co/ecker/vall-e/resolve/main/ckpt/ar%2Bnar-retnet-8/fp32.pth"
-wget -P ./training/valle/data.h5 "https://huggingface.co/ecker/vall-e/resolve/main/data.h5"
-wget -P ./training/valle/config.yaml "https://huggingface.co/ecker/vall-e/raw/main/config.yaml"
+wget -P ./training/valle/ckpt/ar+nar-retnet-8/ "https://huggingface.co/ecker/vall-e/resolve/main/ckpt/ar%2Bnar-retnet-8/fp32.pth"
+wget -P ./training/valle/ "https://huggingface.co/ecker/vall-e/resolve/main/data.tar.gz"
+wget -P ./training/valle/ "https://huggingface.co/ecker/vall-e/raw/main/config.yaml"
+
+tar -xzf ./training/valle/data.tar.gz -C "./training/valle/"
